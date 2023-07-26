ce2e592b8f40dd1fea30d66840c605e86da50529
nakorndevop <138006821+nakorndevop@users.noreply.github.com>
2023-07-26 09:06:39 +0700

    2 files modified

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 // Import stylesheets
 import './style.css';
+
+liff.init({
+  liffId: '2000215406-oapqAqqk', // Use own liffId
+  withLoginOnExternalBrowser: true, // Enable automatic login process
+})
+  .then(() => {
+      // start to use LIFF's api
+  })
+  .catch((err) => {
+      console.log(err);
+  });
